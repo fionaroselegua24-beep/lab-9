e908f8b102097c379003b0131a386dac54ad15b1
fionaroselegua24-beep <fionaroselegua24@gmail.com>
2025-12-15 15:27:57 +0800

    Refactor package name in InvoiceRepository

--- Repository/InvoiceRepository.java
+++ Repository/InvoiceRepository.java
@@ -1,7 +1,8 @@
-package com.grefiel.lab_7.Entity.Repository;
+package com.LEGUA.lab_7.Entity.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.alabado.lab7.entity.Invoice;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 }
+
